ae8cb51bf6c172d7f21e6e7f428d04ec501dd8eb
Ефимов Александр <a.efimov@inenergy.ru>
2020-08-20 12:16:49 +0300

    add protection against infinite loop

--- src/utils/serial.js
+++ src/utils/serial.js
@@ -37,6 +37,8 @@ function sendCommand([byte1, byte2]) {
   }
 }
 
+let failedAttempts = 0;
+
 function writeCommandFromQueue() {
   if (!commandQueue.length) {
     portBusy = false;
@@ -47,10 +49,11 @@ function writeCommandFromQueue() {
   serial.write(cmd);
   serial.once('data', (buf) => {
     console.log('Recieved answer:', buf);
-    if (!buf.toString('ascii').startsWith('ok')) {
+    if (!buf.toString('ascii').startsWith('ok') && failedAttempts < 2) {
       commandQueue.unshift(cmd);
-      writeCommandFromQueue();
-    }
+      failedAttempts++;
+    } else failedAttempts = 0;
+    writeCommandFromQueue();
   });
 }
 
